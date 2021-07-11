ad548101b0f7d4e34ab785cb408f143180e30801
AlexNexton <cianoc13@gmail.com>
2021-07-11 12:42:28 +0000

    js: added the fundamental functions needed for project

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswers(){
+
+}
+
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
+
+function displayDivitionQuestion(){
+
+}
+
+
+
